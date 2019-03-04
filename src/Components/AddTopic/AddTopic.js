5d92541b71a00ee596428b4e2afffdf8aabfcd97
tibi.aki.tivadar@gmail.com <tibi.aki.tivadar@gmail.com>
2019-03-04 16:30:39 +0000

    palette is now clickable and changes state

--- src/Components/AddTopic/AddTopic.js
+++ src/Components/AddTopic/AddTopic.js
@@ -3,12 +3,38 @@ import React, { Component } from "react";
 import "./AddTopic.css";
 
 export default class AddTopic extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { selectedColor: 0 };
+    }
+
+    handlePaletteClick(ind) {
+        let newState = this.state;
+
+        newState.selectedColor = ind;
+
+        this.setState(newState);
+        console.log(ind, " CLICKED", newState);
+    }
+
     renderPalette() {
         const
             colors = ["#ffffff", "#ffff79", "#ffca79", "#ff9dce", "#ff8080", "#ff80ff", "#bdbbff", "#9fffff", "#b3ffb3", "#d6adad"],
-            palette = colors.map((co, i, _, className = "add-topic__color" + (!i ? " add-topic__color--selected" : "")) =>
-                <div className={className} key={i} style={{ "backgroundColor": co }} >&#10004;</div>
-            );
+            palette = colors.map((co, i) => {
+                const
+                    className = "add-topic__color" + (i === this.state.selectedColor ? " add-topic__color--selected" : ""),
+                    key = "palette" + i;
+
+                return (
+                    <div
+                        className={className}
+                        key={key}
+                        style={{ "backgroundColor": co }}
+                        onClick={() => this.handlePaletteClick(i)}
+                    >&#10004;</div>
+                );
+            });
 
         return palette;
     } // end of renderPalette
