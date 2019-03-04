3800db5870009d1d2c0a7522903198ec34a89af7
tibi.aki.tivadar@gmail.com <tibi.aki.tivadar@gmail.com>
2019-03-04 16:05:13 +0000

    select the first color

--- src/Components/AddTopic/AddTopic.js
+++ src/Components/AddTopic/AddTopic.js
@@ -3,6 +3,16 @@ import React, { Component } from "react";
 import "./AddTopic.css";
 
 export default class AddTopic extends Component {
+    renderPalette() {
+        const
+            colors = ["#ffffff", "#ffff79", "#ffca79", "#ff9dce", "#ff8080", "#ff80ff", "#bdbbff", "#9fffff", "#b3ffb3", "#d6adad"],
+            palette = colors.map((co, i, _, className = "add-topic__color" + (!i ? " add-topic__color--selected" : "")) =>
+                <div className={className} key={i} style={{ "backgroundColor": co }} >&#10004;</div>
+            );
+
+        return palette;
+    } // end of renderPalette
+
     render() {
         return (
             <div className="add-topic">
@@ -10,6 +20,18 @@ export default class AddTopic extends Component {
                     Add topic
 
                     <button id="cancel-add-topic-btn">&times;</button>
+                </div>
+                <div className="add-topic__body">
+                    <div className="add-topic__palette">{this.renderPalette()}</div>
+
+                    <div className="add-topic__name">
+                        <input
+                            id="add-topic__name-input"
+                            placeholder="Add topic name"
+                        />
+
+                        <button id="add-topic__create-btn">Create topic</button>
+                    </div>
                 </div>
             </div>
         ); // end of return
