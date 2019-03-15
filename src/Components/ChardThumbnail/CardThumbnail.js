c69ef687c4c05f627a76eec0ee9af16a367f7f7f
tibi.aki.tivadar@gmail.com <tibi.aki.tivadar@gmail.com>
2019-03-15 13:23:14 +0000

    add eventlisteners for showiing

--- src/Components/ChardThumbnail/CardThumbnail.js
+++ src/Components/ChardThumbnail/CardThumbnail.js
@@ -4,12 +4,36 @@ import "./CardThumbnail.css";
 import TopicLabel from "../TopicLabel/TopicLabel";
 
 export default class CardThumbnail extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            "buttonsVisible": false
+        }; // end of state declaration 
+    } // end of constructor
+
+
+    showButtons(show) {
+        const newState = this.state;
+
+        newState.buttonsVisible = show;
+
+        this.setState(newState);
+    } // end of showButtons
+
+
+
     render() {
         const card = JSON.parse(localStorage.cards).filter(c => c.id === this.props.id)[0];
 
         console.log(card);
         return (
-            <div className="card-thumbnail">
+            <div
+                className="card-thumbnail"
+                onMouseOver={() => this.showButtons(true)}
+                onMouseOut={() => this.showButtons(false)}
+                onClick={() => this.showButtons(this.state.buttonsVisible ? false : true)}
+            >
                 <div className="card-thumbnail__header">
                     {
                         card.topics.map(topic => {
@@ -20,7 +44,12 @@ export default class CardThumbnail extends Component {
                     }
                     <span>{Math.round((card.results[0] / card.results[1]) * 100)}% id:{card.id}</span>
                 </div>
+
                 <div className="card-thumbnail__body">{card.question}</div>
+
+                {this.state.buttonsVisible && <button className="card-thumbnail__edit">edit</button>}
+
+                {this.state.buttonsVisible && <button className="card-thumbnail__remove">remove</button>}
             </div>
         );
     } // end of render
