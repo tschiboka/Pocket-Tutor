297d023a11462149a4b1061bee3a9e379a2201b4
tibi.aki.tivadar@gmail.com <tibi.aki.tivadar@gmail.com>
2019-03-18 21:40:11 +0000

    multi slider thumbs and active rail are working

--- src/Components/RangeWithTwoSliders/RangeWithTwoSliders.js
+++ src/Components/RangeWithTwoSliders/RangeWithTwoSliders.js
@@ -7,8 +7,83 @@ export default class RangeWithTwoSliders extends Component {
         super(props);
 
         const { min, max } = { ...props };
-        this.state = { min, max };
+        this.state = {
+            min, max,
+            "mouseDown": false,
+        };
     } // end of constructor
+
+
+
+    handleMouseDown(target, e) {
+        const newState = this.state;
+
+        newState.mouseDown = target;
+
+        if (target === "min") { newState.minStartX = e.clientX; }
+
+        if (target === "max") { newState.maxStartX = e.clientX; }
+        this.setState(newState);
+    } // end of handleMouseDown
+
+
+    resetMouseDown() {
+        const newState = this.state;
+
+        // reset
+        newState.mouseDown = false;
+
+        this.setState(newState);
+    } // resetMouseDown
+
+
+
+    handleMouseMove(e) {
+        e.preventDefault();
+
+        // SET THUMBS
+        if (this.state.mouseDown === "min") {
+            const // get slide and mouse values
+                slider = document.getElementById(this.props.id + "__slider--min"),
+                mouseX = e.clientX,
+                track = document.getElementById(this.props.id + "__track"),
+                trackX = Math.round(track.getBoundingClientRect().x),
+                diffX = mouseX - trackX;
+
+            slider.style.left = diffX + "px";
+        } // end of if min is being slide
+
+        if (this.state.mouseDown === "max") {
+            const // get slide and mouse values
+                slider = document.getElementById(this.props.id + "__slider--max"),
+                mouseX = e.clientX,
+                track = document.getElementById(this.props.id + "__track"),
+                trackX = Math.round(track.getBoundingClientRect().x),
+                diffX = mouseX - trackX;
+
+            slider.style.left = diffX + "px";
+        } // end of if min is being slide
+
+        // SET ACTIVE TRACK
+        if (this.state.mouseDown) {
+            const
+                box = document.getElementById(this.props.id + "__track"),
+                boxX = Math.round(box.getBoundingClientRect().x),
+                track = document.getElementById(this.props.id + "__active-track"),
+                minThumb = document.getElementById(this.props.id + "__slider--min"),
+                minRect = minThumb.getBoundingClientRect(),
+                trackStart = minRect.x + (minRect.width / 2) - boxX,
+                maxThumb = document.getElementById(this.props.id + "__slider--max"),
+                maxRect = maxThumb.getBoundingClientRect(),
+                trackEnd = maxRect.x + (maxRect.width / 2) - boxX,
+                trackWidth = Math.max(trackStart, trackEnd) - Math.min(trackStart, trackEnd);
+
+            track.style.width = trackWidth + "px";
+            track.style.left = trackStart + "px";
+
+            console.log();
+        } // end of if there was a mouseDown
+    } // end of handleMouseMove
 
 
 
@@ -19,12 +94,29 @@ export default class RangeWithTwoSliders extends Component {
 
                 <div className="range__body">
 
-                    <div className="range__body__track">
-                        <div className="range__body__thumb"></div>
+                    <div
+                        className="range__body__track"
+                        id={this.props.id + "__track"}
+                        onMouseLeave={() => this.resetMouseDown()}
+                        onMouseUp={() => this.resetMouseDown()}
+                        onMouseMove={e => this.handleMouseMove(e)}
+                    >
+                        <div
+                            className="range__body__active-track"
+                            id={this.props.id + "__active-track"}
+                        ></div>
 
-                        <div className="range__body__slider--min"></div>
+                        <div
+                            className="range__body__slider--min"
+                            id={this.props.id + "__slider--min"}
+                            onMouseDown={e => this.handleMouseDown("min", e)}
+                        ></div>
 
-                        <div className="range__body__slider--max"></div>
+                        <div
+                            className="range__body__slider--max"
+                            id={this.props.id + "__slider--max"}
+                            onMouseDown={e => this.handleMouseDown("max", e)}
+                        ></div>
                     </div>
                 </div>
 
