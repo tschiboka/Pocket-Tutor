c1712e2cdd07e75d9b5d2fa3a4572102e45e824f
tibi.aki.tivadar@gmail.com <tibi.aki.tivadar@gmail.com>
2019-03-19 14:29:17 +0000

    slider thumbs are pushing each other

--- src/Components/RangeWithTwoSliders/RangeWithTwoSliders.js
+++ src/Components/RangeWithTwoSliders/RangeWithTwoSliders.js
@@ -43,38 +43,64 @@ export default class RangeWithTwoSliders extends Component {
         if (this.state.mouseDown) {
             e.preventDefault();
 
-            // get ten percent for push effect
             const
                 track = document.getElementById(this.props.id + "__track"),
                 trackRect = track.getBoundingClientRect(),
                 minThumb = document.getElementById(this.props.id + "__slider--min"),
                 minRect = minThumb.getBoundingClientRect(),
                 maxThumb = document.getElementById(this.props.id + "__slider--max"),
-                maxRect = maxThumb.getBoundingClientRect();
+                maxRect = maxThumb.getBoundingClientRect(),
+                sliderWidth = document.getElementById(this.props.id + "__slider--min").getBoundingClientRect().width,
+                percent = (trackRect.width - sliderWidth) / 100,
+                sliderMin = document.getElementById(this.props.id + "__slider--min"),
+                sliderMax = document.getElementById(this.props.id + "__slider--max");
 
 
             // SET THUMBS
+
             // MIN THUMB
             if (this.state.mouseDown === "min") {
-                const // get slide and mouse values
-                    slider = document.getElementById(this.props.id + "__slider--min"),
+                const // get track and mouse values
                     mouseX = e.clientX,
-                    trackX = Math.round(track.getBoundingClientRect().x),
-                    diffX = mouseX - trackX;
+                    trackX = Math.round(trackRect.x),
+                    diffX = mouseX - trackX,
+                    newMinDigit = Math.round(diffX / percent),
+                    newMaxDigit = 100 - Math.round(Number((maxThumb.style.right || "0").match(/\d+/g)[0]) / percent);
 
-                slider.style.left = diffX + "px";
+                // min slider won't go over 80
+                if (newMinDigit <= 80) {
+                    // SET MIN SLIDER 
+                    sliderMin.style.left = diffX + "px";
+
+                    // PUSH MAX SLIDER
+                    if (newMaxDigit - newMinDigit <= 20) {
+                        sliderMax.style.right = (100 - (newMinDigit + 20)) * percent + "px";
+                    } // end of if two sliders diff is less than 20
+                } // end of min is under 80
             } // end of if min is being slided
 
 
             // MAX THUMB
             if (this.state.mouseDown === "max") {
-                const // get slide and mouse values
-                    slider = document.getElementById(this.props.id + "__slider--max"),
+                const // get track and mouse values
                     mouseX = e.clientX,
                     trackX = Math.round(trackRect.x),
-                    diffX = trackRect.width - (mouseX - trackX);
+                    diffX = trackRect.width - (mouseX - trackX),
+                    newMaxDigit = 100 - (Math.round(diffX / percent)),
+                    newMinDigit = Math.round(Number((minThumb.style.left || "0").match(/\d+/g)[0]) / percent);
 
-                slider.style.right = diffX + "px";
+
+                // max slider won't go under 20
+                if (newMaxDigit >= 20) {
+                    // SET MAX SLIDER 
+                    sliderMax.style.right = (100 - newMaxDigit) * percent + "px";
+
+
+                    // PUSH MIN SLIDER
+                    if (newMaxDigit - newMinDigit <= 20) {
+                        sliderMin.style.left = (newMaxDigit - 20) * percent + "px";
+                    } // end of if two sliders diff is less than 20
+                } // end of max is over 20
             } // end of if max is being slided
 
 
@@ -93,15 +119,11 @@ export default class RangeWithTwoSliders extends Component {
 
             // DISPLAY RANGE DIGITS
             const
-                sliderWidth = document.getElementById(this.props.id + "__slider--min").getBoundingClientRect().width,
-                percent = (trackRect.width - sliderWidth) / 100,
                 minDigit = Math.round(Number((minThumb.style.left || "0").match(/\d+/g)[0]) / percent),
                 maxDigit = 100 - Math.round(Number((maxThumb.style.right || "0").match(/\d+/g)[0]) / percent);
 
             document.getElementById(this.props.id + "min-text").innerHTML = minDigit;
             document.getElementById(this.props.id + "max-text").innerHTML = maxDigit;
-
-            console.log(maxThumb.style.rigth);
         } // end of if mouseDown
     } // end of handleMouseMove
 
