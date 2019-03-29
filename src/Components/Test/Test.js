fe0c39a20c2e01b63e5ad49abba124b13ca76933
tibi.aki.tivadar@gmail.com <tibi.aki.tivadar@gmail.com>
2019-03-29 13:38:28 +0000

    basic topic selection structure on test

--- src/Components/Test/Test.js
+++ src/Components/Test/Test.js
@@ -8,7 +8,23 @@ export default class Test extends Component {
             this.props.visible &&
             <div className="test">
                 <div className="test__header">Test settings</div>
+
+                <div className="test__topics-box">
+                    <div className="test__available-topics-box">
+                        <div className="test__available-topics__header">Topics</div>
+                    </div>
+
+                    <div className="test__topics-btn-box">
+                        <button id="test__select-btn">&gt;</button>
+
+                        <button id="test__deselect-btn">&lt;</button>
+                    </div>
+
+                    <div className="test__selected-topic-box">
+                        <div className="test__selected-topics__header">Selected</div>
+                    </div>
+                </div>
             </div>
-        );
+        ); // end of return
     } // end of render
 } // end of Test
