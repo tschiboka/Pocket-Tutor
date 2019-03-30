e4304bf9df7a3a0958736b8760d2ff6d9c06973d
tibi.aki.tivadar@gmail.com <tibi.aki.tivadar@gmail.com>
2019-03-30 03:19:55 +0000

    render test topics

--- src/Components/Test/Test.js
+++ src/Components/Test/Test.js
@@ -2,7 +2,37 @@ import React, { Component } from "react";
 
 import "./Test.css";
 
+import TopicLabel from "../TopicLabel/TopicLabel";
+
 export default class Test extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            "availableTopics": JSON.parse(localStorage.topics),
+            "selectedTopics": []
+        } // end of state declaration
+    } // end of constructor
+
+
+
+    renderTopics(topicSelector) {
+        return (
+            <ul className={"test__topic-list--" + topicSelector}>
+                {this.state[topicSelector].map((topic, i) =>
+                    <li className="test__topic">
+                        <TopicLabel
+                            text={topic.name}
+                            color={topic.color}
+                            key={i} />
+                    </li>
+                ) /* end of topicSelector map*/}
+            </ul>
+        ); // end of return list
+    } // end of renderTopics
+
+
+
     render() {
         return (
             this.props.visible &&
@@ -12,6 +42,8 @@ export default class Test extends Component {
                 <div className="test__topics-box">
                     <div className="test__available-topics-box">
                         <div className="test__available-topics__header">Topics</div>
+
+                        <div className="test__available-topics">{this.renderTopics("availableTopics")}</div>
                     </div>
 
                     <div className="test__topics-btn-box">
@@ -22,6 +54,8 @@ export default class Test extends Component {
 
                     <div className="test__selected-topic-box">
                         <div className="test__selected-topics__header">Selected</div>
+
+                        <div className="test__selected-topics">{this.renderTopics("selectedTopics")}</div>
                     </div>
                 </div>
             </div>
