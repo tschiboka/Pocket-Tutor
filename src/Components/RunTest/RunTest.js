3fcc4ad6221f80333e81cdff5f2f373ba7f5d6d2
tibi.aki.tivadar@gmail.com <tibi.aki.tivadar@gmail.com>
2019-06-11 10:42:54 +0100

    run test progress bar

--- src/Components/RunTest/RunTest.js
+++ src/Components/RunTest/RunTest.js
@@ -6,7 +6,13 @@ export default class RunTest extends Component {
     render() {
         return (
             this.props.visible &&
-            <div>Runtest</div>
+            <div className="run-test-box">
+                <div className="run-test__progress-box">
+                    <div className="run-test__progressbar">
+                        <div className="run-test__progress"></div>
+                    </div>
+                </div>
+            </div>
         ); // end of return
     } // end of render
 } // end of RunTest
