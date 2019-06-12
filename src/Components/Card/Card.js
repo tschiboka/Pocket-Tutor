8d5f52eb18e275e90be654e7bc8aae7e7a036907
tibi.aki.tivadar@gmail.com <tibi.aki.tivadar@gmail.com>
2019-06-12 11:09:28 +0100

    display the four cards visible for animation

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -3,9 +3,21 @@ import React, { Component } from "react";
 import "../Card/Card.css";
 
 export default class Card extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        } // end of state declaration
+    } // end of constructor
+
+
+
     render() {
+        const CARD = this.props.card || { "id": -1, "question": "", "answer": "", "results": [0, 0], "topics": [""] };
+        console.log(CARD);
         return (
-            <div className="card__card-box">CARD</div>
+            <div className="card__card-box">{CARD.question}</div>
         ); // end of return
     } // end of render
 } // end of Card Component
--- src/Components/RunTest/RunTest.js
+++ src/Components/RunTest/RunTest.js
@@ -5,6 +5,39 @@ import "../RunTest/RunTest.css";
 import Card from "../Card/Card";
 
 export default class RunTest extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            "current": 0
+        } // end of state declaration
+    } // end of constructor
+
+
+
+    // true/false store result
+    assesCard(result) {
+        // change state
+        const newState = this.state;
+        newState.current++;
+        this.setState(newState);
+
+        console.log("RESULT", result);
+
+        // here are the changes on the localstore card objetc
+
+        // roll the cards and animate
+        this.rollCard();
+    } // end of assesCard
+
+
+
+    rollCard() {
+
+    } // end of rollCard
+
+
+
     render() {
         return (
             this.props.visible &&
@@ -17,21 +50,29 @@ export default class RunTest extends Component {
 
                 <div className="run-test__test-box">
                     <div className="run-test__cards">
-                        <div className="run-test__prev-card"><Card /></div>
+                        <div id="run-test__prev-card-div">
+                            <Card card={this.props.cards[this.state.current - 1]} />
+                        </div>
 
-                        <div className="run-test__curr-card"><Card /></div>
+                        <div id="run-test__curr-card-div">
+                            <Card card={this.props.cards[this.state.current]} />
+                        </div>
 
-                        <div className="run-test__next-card"><Card /></div>
+                        <div id="run-test__next-card-div">
+                            <Card card={this.props.cards[this.state.current + 1]} />
+                        </div>
 
-                        <div className="run-test__next2-card"><Card /></div>
+                        <div id="run-test__nex2-card-div">
+                            <Card card={this.props.cards[this.state.current + 2]} />
+                        </div>
                     </div>
 
                     <div className="run-test__btn-box">
-                        <button>&times;</button>
+                        <button onClick={() => this.assesCard(false)}>&times;</button>
 
                         <button>Turn</button>
 
-                        <button>&#10003;</button>
+                        <button onClick={() => this.assesCard(true)}>&#10003;</button>
                     </div>
                 </div>
             </div>
