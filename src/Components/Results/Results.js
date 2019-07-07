a0853507fbcf5ec9efe8a1f5318348eaad4b9176
tibi.aki.tivadar@gmail.com <tibi.aki.tivadar@gmail.com>
2019-07-07 01:00:09 +0100

    create an array of results objects

--- src/Components/Results/Results.js
+++ src/Components/Results/Results.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 
 import "../Results/Results.css";
+import RoundProgress from "../RoundProgress/RoundProgress";
 
 export default class Results extends Component {
     constructor(props) {
@@ -11,12 +12,23 @@ export default class Results extends Component {
             ctops = cards.map(c => c.topics),           // extract topics (nested array)
             topic = [...new Set([].concat(...ctops))],  // flatten and reduce duplicates
             resul = topic.map(t => {
-                //const that = cards.map(c => c.topics.find(""));
-                //console.log(that);
-            });
+                const
+                    allResultOnTopic = cards
+                        .map(c => c.topics              // get cards
+                            .find(to => to === t) ? c.results : [0, 0]) // find topic in cards and get results
+                        .reduce((prev, curr) => [prev[0] + curr[0], prev[1] + curr[1]]), // add results together
+                    precentage = allResultOnTopic[0]    // calculate pecentage from [success, all]
+                        ? Math.round((allResultOnTopic[0] / allResultOnTopic[1]) * 100)
+                        : 0,                            // else 0
+                    color = JSON.parse(localStorage.topics) // get localStorage topics
+                        .find(to => to.name === t).color;   // find corrisponding color
 
-        console.log(cards, topic);
+                return { "topicname": t, "color": color, "percent": precentage };
+            }); // end of topic.map
+
+        console.log(resul);
         this.state = {
+            "results": resul
         }
     } // end of constructor
 
@@ -28,7 +40,9 @@ export default class Results extends Component {
                 <div className="results__header">RESULTS</div>
 
                 <div className="results__body">
-                    {}
+                    {
+                        //this.state.results.map(  ))
+                    }
                 </div>
 
                 <div className="results__footer">
